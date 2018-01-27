b8aa6493ec53b33fd1995f3ccc94806270a940c5
Valerio Riva <valerio.riva@it.ibm.com>
2018-01-27 12:43:57 +0100

    player prefabs

--- Assets/Scripts/Player/Board.cs
+++ Assets/Scripts/Player/Board.cs
@@ -10,25 +10,41 @@ public class Board : Singleton<Board> {
 	void Awake() {
         for (int y = 0; y < rows; y++) {
             GameObject row = new GameObject(y + " row");
-            row.transform.parent = this.transform;
-            row.transform.localScale = Vector3.zero;
+            row.transform.SetParent(this.transform);
+            row.transform.localScale = Vector3.one;
             HorizontalLayoutGroup hlg = row.AddComponent<HorizontalLayoutGroup>();
             hlg.childControlWidth = true;
             hlg.childControlHeight = true;
             hlg.childAlignment = TextAnchor.MiddleCenter;
             for(int i = 0; i < GameManager.SPACE_SIZE; i++) {
                 GameObject cell = new GameObject(y + "-" + i + " cell");
-                cell.transform.parent = row.transform;
-                cell.transform.localScale = Vector3.zero;
+                cell.AddComponent<RectTransform>();
+                cell.transform.SetParent(row.transform);
+                cell.transform.localScale = Vector3.one;
 			}
 		}
 
-        List<ISector> WorldMap = GameManager.Instance.WorldMap;
+        List<ISector> WorldMap = new List<ISector>(){
+            new SparseAsteroidSector(),
+            new BlackAlienSector(),
+            new BlackHoleSector(),
+            new SparseAsteroidSector(),
+            new BlackAlienSector(),
+            new SparseAsteroidSector(),
+            new BlackAlienSector(),
+            new BlackHoleSector(),
+            new BlackHoleSector(),
+            new BlackHoleSector(),
+        };
+        if (GameManager.Instance != null) {
+            WorldMap = GameManager.Instance.WorldMap;    
+        }
         List<ISector> LocalMap = new List<ISector>(WorldMap.Count);
         for (int i = 0; i < WorldMap.Count; i++) {
             LocalMap.Add(WorldMap[i].Clone());
         }
-                   
+
+
 	}
 
 	// Use this for initialization
