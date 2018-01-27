9f90fc1473459148d94e99c26ef7cb0d4d77e34e
Augusto Pace <augusto@motorsportgaming.com>
2018-01-27 11:39:03 +0100

    ...

---
+++ Assets/Scripts/UI/UILaunchTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILaunchTimer : MonoBehaviour {
+
+    Text _timer;
+
+    void Awake () {
+        _timer = GetComponent<Text>();
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        if ( GameManager.IsInstanced ) {
+            _timer.text = GameManager.Instance.LaunchTimerInt.ToString();
+        }
+	}
+}
