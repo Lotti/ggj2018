75c65aeb6b7026be5e11a595dae2337abbbb6497
Matteo Vilardo MACBOOK PRO <matteovilar@hotmail.it>
2018-01-27 12:35:36 +0100

    Aggiunto uno scene manager, aggiunti bottoni sul menu principale, aggiunta classe GGJESTENSIONIIIII!! ehh niente

---
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public enum Scenes
+{
+    Main,
+    StartScene
+}
+
+public class SceneManager : Singleton<SceneManager>
+{
+    public Scenes CurrentScene
+    {
+        get
+        {
+            return scene;
+        }
+    }
+
+    private Scenes scene = Scenes.StartScene;
+
+    public void ChangeScene(Scenes scene)
+    {
+        this.scene = scene;
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene.ScenesToString());
+    }
+
+    private void Awake()
+    {
+        this.scene = Scenes.StartScene;
+    }
+}
