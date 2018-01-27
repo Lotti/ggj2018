54210a3a102fe7661bec8e66bb55467940887407
Ennio DELL XPS 13 <ennio@ambiensvr.com>
2018-01-27 21:39:24 +0100

    Aggiunti pulsanti, spawn dei foglietti

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,9 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour 
+public class UIManager : Singleton<UIManager> 
 {
-    
-	
+    public GameObject PaperLogPrefab;
+    public Transform PaperLogStartTransform;
+    public Transform PaperLogFirstAnimationTranform;
+
+    public List<GameObject> ScartoffieSpawnate;
+
+    private void Awake()
+    {
+        this.ScartoffieSpawnate = new List<GameObject>();
+        this.GeneratePaperLog("AAAHAHAHAHAHAH", null);
+    }
+
+    public void GeneratePaperLog(string log, Action oncomplete)
+    {
+        var go = GameObject.Instantiate(this.PaperLogPrefab, this.PaperLogStartTransform);
+        go.transform.localPosition = Vector3.zero;
+        var pt = go.GetComponent<PaperThing>();
+        pt.SetText(log);
+        pt.StartAnimation(
+            this.PaperLogFirstAnimationTranform,
+            oncomplete);
+        
+        this.ScartoffieSpawnate.Add(go);
+
+    }
 }
---
+++ Assets/Scripts/UI/StartButton.cs
@@ -0,0 +1,28 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : A3DClickable
+{
+
+    public static Color activeColor = Color.green;
+    public static Color nonActiveColor = Color.red;
+
+    public int Tick = 0;
+
+    //public Light buttonLight;
+
+    public bool isActive = false;
+
+    public override void OnClick()
+    {
+        this.isActive = !this.isActive;
+        //this.buttonLight.gameObject.SetActive(this.isActive);
+
+        //this.rend.GetComponent<Renderer>().material.SetColor("_Color", (this.isActive ? activeColor : nonActiveColor));
+
+        this.transform.DOLocalMoveY(((!this.isActive) ? 2.079f : 1.969f), 1);
+
+    }
+}
