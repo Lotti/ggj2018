172a8d13f76312c82b68645c3e716d5f80c21552
Ennio DELL XPS 13 <ennio@ambiensvr.com>
2018-01-28 10:16:57 +0100

    Un po' di fixini alla televisione

---
+++ Assets/TVManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVManager : Singleton<TVManager>
+{
+    public GameObject razzo;
+    public GameObject sbarre;
+    
+    public void ShowRazzoCazzo()
+    {
+        if (!razzo.activeSelf)
+        {
+            razzo.SetActive(true);
+            sbarre.SetActive(false);
+        }
+    }
+    public void ShowSbarre()
+    {
+        if (razzo.activeSelf)
+        {
+            razzo.SetActive(false);
+            sbarre.SetActive(true);
+        }
+    }
+
+}
