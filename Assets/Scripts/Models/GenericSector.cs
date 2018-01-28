10c292a4885d03cdc16ffce3f8c0ee6d36333480
Valerio Riva <valerio.riva@it.ibm.com>
2018-01-28 11:19:34 +0100

    Ancora non salva i prefab separatamente dal salvataggio della scena!! E che cazzo!

--- Assets/Scripts/Models/GenericSector.cs
+++ Assets/Scripts/Models/GenericSector.cs
@@ -34,7 +34,9 @@ public class GenericSectorCalculator
             tempBonus = GameManager.Instance.tempBonus;
         }
 
+        Debug.Log("SectorTemperature: " +SectorTemperature+ " modSpaceship: "+modSpaceship+" tempBonus: "+tempBonus);
         float app = SectorTemperature - (modSpaceship + tempBonus);
+        Debug.Log("= "+app);
 
         if (app > 0)
         {
