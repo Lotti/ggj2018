1af169673ad7efc47d1aaf449f6ec86b8c875b15
bubez <mneddi@gmail.com>
2018-01-28 10:46:46 +0100

    Added audio manager (to be completed ma devo andare un attimo via, eh - se qualcuno vuole continuare che lo faccia, altrimenti a dopo).

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    private AudioSource music;
+    private AudioSource soundEffect;
+
+    [SerializeField]
+    private AudioClip mainTheme;
+    [SerializeField]
+    private AudioClip menuButtonPressedSound;
+    [SerializeField]
+    private AudioClip launchButtonPressedSound;
+    [SerializeField]
+    private AudioClip ingameButtonPressedSound;
+    [SerializeField]
+    private AudioClip shipLaunchedSound;
+    [SerializeField]
+    private AudioClip paperMovedSound;
+    [SerializeField]
+    private AudioClip printerSound;
+
+    public void PlayMainTheme()
+    {
+        music.clip = mainTheme;
+        music.loop = true;
+        music.Play();
+    }
+
+    public void PlayMenuButtonPressedSound()
+    {
+        soundEffect.PlayOneShot(menuButtonPressedSound);
+    }
+
+    public void PlayLaunchButtonPressedSound()
+    {
+        soundEffect.PlayOneShot(launchButtonPressedSound);
+    }
+
+    public void PlayIngameButtonPressedSound()
+    {
+        soundEffect.PlayOneShot(ingameButtonPressedSound);
+    }
+
+    public void PlayShipLaunchedSound()
+    {
+        soundEffect.PlayOneShot(shipLaunchedSound);
+    }
+
+    public void PlayPaperMovedSound()
+    {
+        soundEffect.PlayOneShot(paperMovedSound);
+    }
+
+    public void PlayPrinterSound()
+    {
+        soundEffect.PlayOneShot(printerSound);
+    }
+
+    public void StopMusic()
+    {
+        music.Stop();
+    }
+}
